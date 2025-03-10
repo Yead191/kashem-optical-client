c8f4b5d03ea5a72d8fd46a6eef297802520f9dec
Yead <yead19@gmail.com>
2025-03-11 05:01:37 +0600

    simple modification

--- src/Pages/Dashboard/AdminRoute/ManageUsers.jsx
+++ src/Pages/Dashboard/AdminRoute/ManageUsers.jsx
@@ -54,15 +54,36 @@ const ManageUsers = () => {
                 </h2>
             </div>
             <div className='container mx-auto'>
-                <Input
-                    className={clsx(
-                        'my-3 block w-full md:w-56 rounded-lg border bg-black/5 py-1.5 px-3 text-sm/6 text-black',
-                        'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+                <div className="relative flex items-center gap-2">
+                    <Input
+                        className={clsx(
+                            "my-3 block w-full sm:w-full md:w-56 rounded-lg border bg-black/5 py-1.5 px-3 pr-10 text-sm text-black",
+                            "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
+                        )}
+                        placeholder="Search User"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    {searchTerm && (
+                        <button
+                        title='Clear Search'
+                            className='btn btn-sm text-red-500 hidden md:flex'
+                            onClick={() => setSearchTerm("")}
+                        >
+                            ✕
+                        </button>
                     )}
-                    placeholder='Search User'
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
+                    {searchTerm && (
+                        <button
+                            className="absolute right-3 md:hidden top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                            onClick={() => setSearchTerm("")}
+                        >
+                            ✕
+                        </button>
+                    )}
+                </div>
+
+
                 <div className="overflow-x-auto">
                     <table className="table">
                         {/* head */}
