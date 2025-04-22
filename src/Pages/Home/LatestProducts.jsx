9408e70157adfc32f2d65863a6759d30090aa9e3
Yead191 <yead191@gmail.com>
2025-04-22 21:19:23 +0600

    latest product bug fix

--- src/Pages/Home/LatestProducts.jsx
+++ src/Pages/Home/LatestProducts.jsx
@@ -83,7 +83,7 @@ function LatestProducts() {
         <h2 className="text-3xl font-bold text-start mb-8">
           Check Our Latest Collections
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 max-xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4">
           {products?.map((product) => (
             <ProductCard key={product.productId} product={product} />
           ))}
